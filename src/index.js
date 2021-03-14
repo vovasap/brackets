6414829002f656f5184c7d6110e6bfa63b4b353d
vovasap <vovasap.pblc@gmail.com>
2021-03-14 23:29:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let checks = (new Array(bracketsConfig.length)).fill(false)
+    while(checks.includes(false)) {
+        bracketsConfig.forEach((conf, i) => {
+            const brackets = conf.join('')
+            if (str.includes(brackets)){
+                str = str.replace(brackets, '')
+                checks[i] = false
+            } else {
+                checks[i] = true
+            }
+        })
+        if(str.length === 0) return true
+    }
+    return false
 }
